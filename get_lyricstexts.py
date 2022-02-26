daf11f3ff8fb515c44ff9b5a7240c735e2d89066
Shen Shen <8396493+shensquared@users.noreply.github.com>
2022-02-25 19:16:24 -0500

    format/cleanup lyricsgenius's return

--- get_lyricstexts.py
+++ get_lyricstexts.py
@@ -12,7 +12,17 @@ def get_texts(title, artist):
     #  dump the texts; seems a bit difficult. Alternatively, paste over in terminal.
     song = genius.search_song(clean_title, artist)
     l = song.lyrics
-    # TODO: clean up text format
+    # TODO: clean up text format. Cleaned, but why were these so ugly added anyway?
+    # remove the song title
+    if l.startswith(song.title_with_featured):
+        l = l.replace(song.title_with_featured, '')
+    else:
+        l = l.replace(song.title, '')
+    # remove the prefix word 'Lyrics'
+    l=l[7:]
+    if l.endswith('Embed'):
+        l = l.replace('Embed', '')
+
     lines = l.split('\n')
     lines = [line for line in lines if line.strip()]
     return lines
