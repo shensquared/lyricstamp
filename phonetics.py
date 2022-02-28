213d5525c498da249fb1e69875f6bd5a6cbf7b6a
Shen Shen <8396493+shensquared@users.noreply.github.com>
2022-02-27 19:48:57 -0500

    Update phonetics.py

--- phonetics.py
+++ phonetics.py
@@ -1,8 +1,13 @@
 from itertools import chain
 import cutlet
 
-def add_phonetics(lines, lang='JP'):
-    katsu = cutlet.Cutlet()
-    katsu.use_foreign_spelling = False
-    phonetics= ['[tr]'+katsu.romaji(l) for l in lines]
-    return list(chain.from_iterable(zip(lines, phonetics)))
+def add_phonetics(lines, p):
+    if p =='J':
+        katsu = cutlet.Cutlet()
+        katsu.use_foreign_spelling = False
+        phonetics= ['[tr]'+katsu.romaji(l) for l in lines]
+        return list(chain.from_iterable(zip(lines, phonetics)))
+    if p=='Y':
+        pass
+
+    return lines
