262a675f750590dc4e219b72a832192a1410b854
Shen Shen <8396493+shensquared@users.noreply.github.com>
2022-03-01 03:49:05 -0500

    add jyutping for cantonese

--- phonetics.py
+++ phonetics.py
@@ -1,13 +1,15 @@
 from itertools import chain
 import cutlet
+from pyjyutping import jyutping
+
 
 def add_phonetics(lines, p):
-    if p =='J':
+    if p == 'J':
         katsu = cutlet.Cutlet()
         katsu.use_foreign_spelling = False
-        phonetics= ['[tr]'+katsu.romaji(l) for l in lines]
+        phonetics = ['[tr]' + katsu.romaji(l) for l in lines]
         return list(chain.from_iterable(zip(lines, phonetics)))
-    if p=='Y':
-        pass
-
+    if p == 'Y':
+        phonetics = ['[tr]' + jyutping.convert(l) for l in lines]
+        return list(chain.from_iterable(zip(lines, phonetics)))
     return lines
